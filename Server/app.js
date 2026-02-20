ff369bcfb94605e35fc92a444413f85a5d1fa43c
Reevain <patelreevain@example.com>
2026-02-20 13:29:00 +0530

    Fixed CORS for production frontend

--- Server/app.js
+++ Server/app.js
@@ -10,11 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+import cors from 'cors';
+
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://your-frontend-name.vercel.app'
+    'https://eventchecklist-seven.vercel.app/'
   ],
+  methods: ['GET','POST','PUT','DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
   })
 );
